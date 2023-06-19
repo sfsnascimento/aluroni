895512af3671c00edadea02bbe7ef97400c3b79d
Samuel Nascimento <sfsnascimento@gmail.com>
2023-06-19 19:10:57 -0300

    adicionando propriedade dos itens

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,29 +1,38 @@
 import styles from './Item.module.scss';
-import logo from 'assets/logo.svg';
+import cardapio from '../itens.json';
+import classNames from 'classnames';
 
-export default function Item() {
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props) {
+  const { photo, title, description, category: { label }, size, serving, price } = props;
   return (
     <div className={styles.item}>
       <div className={styles.item__imagem}>
-        <img src={logo} alt="imagem" />
+        <img src={photo} alt={title} />
       </div>
       <div className={styles.item__descricao}>
         <div className={styles.item__titulo}>
-          <h2>Macarrão</h2>
-          <p>Descrição do macarrão</p>
+          <h2>{title}</h2>
+          <p>{description}</p>
         </div>
         <div className={styles.item__tags}>
-          <div className={styles.item__tipo}>
-            Massa
+          <div
+            className={classNames({
+              [styles.item__tipo]: true,
+              [styles[`item__tipo__${label.toLocaleLowerCase()}`]]: true
+            })}
+          >
+            {label}
           </div>
           <div className={styles.item__porcao}>
-            400g
+            {size}g
           </div>
           <div className={styles.item__qtdpessoas}>
-            Serve 2 pessoas
+            Serve {serving} pessoa{serving === 1 ? '' : 's'}
           </div>
           <div className={styles.item__valor}>
-            R$ 50,00
+            R$ {price.toFixed(2)}
           </div>
         </div>
       </div>
