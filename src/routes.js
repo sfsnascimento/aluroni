58176cdd0452a56b2c57dc2eb63a2f6ed546bb5d
Samuel Nascimento <sfsnascimento@gmail.com>
2023-06-21 20:05:07 -0300

    adicionando componentes no routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='/cardapio' element={<Cardapio />} /> 
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
